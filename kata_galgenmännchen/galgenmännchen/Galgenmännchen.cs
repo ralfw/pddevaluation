709c2deeea2f025f064dc7b51feb166ad7380e1f
Ralf Westphal <info@ralfw.de>
2018-03-02 17:03:22 +0100

    Improved IOSP conformance #8

--- kata_galgenmännchen/galgenmännchen/Galgenmännchen.cs
+++ kata_galgenmännchen/galgenmännchen/Galgenmännchen.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Linq;
 
 namespace galgenmännchen
 {
@@ -12,16 +13,20 @@ namespace galgenmännchen
         }
 
         // @todo #1 Besser strukturieren nach IOSP
-        public string RateBuchstabe(char buchstabe)
-        {
-            for(var i=0; i<_erratenesWort.Length; i++)
-            {
-                if (_erratenesWort[i] == '-')
-                    _erratenesWort[i] = Check_char(_gesuchtesWort[i], buchstabe);
-            }
+        public string RateBuchstabe(char buchstabe) {
+            Find_missing_char_positions(
+                i => _erratenesWort[i] = Check_char(_gesuchtesWort[i], buchstabe));
             return new string(_erratenesWort);
         }
 
+
+        private void Find_missing_char_positions(Action<int> onMissing) {
+            for(var i=0; i<_erratenesWort.Length; i++)
+                if (_erratenesWort[i] == '-')
+                    onMissing(i);
+        }
+        
+        
         private char Check_char(char wortbuchtabe, char gesuchterBuchstabe)
             => char.ToLower(wortbuchtabe) == char.ToLower(gesuchterBuchstabe) ? wortbuchtabe : '-';
     }
