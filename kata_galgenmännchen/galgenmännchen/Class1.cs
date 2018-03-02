5e225d0a67770713e72ee34fd0f3e566a5373d05
Ralf Westphal <info@ralfw.de>
2018-03-02 09:17:33 +0100

    Projekte für Produktionscode und Tests
    Klassenrumpf gem. Anforderungen

---
+++ kata_galgenmännchen/galgenmännchen/Class1.cs
@@ -0,0 +1,9 @@
+﻿using System;
+
+namespace galgenmännchen
+{
+    public class Galgenmännchen {
+        public Galgenmännchen(string gesuchtesWort) {  }
+        public string RateBuchstabe(char buchstabe) { throw new NotImplementedException(); }
+    }
+}
