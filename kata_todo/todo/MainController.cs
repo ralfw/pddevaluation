b4ba20a72357deedbd8974adee01ce2057053560
Kili2018 <kili@ralfw.de>
2018-03-03 14:45:15 +0100

    projektgerüst. maincontroller.

---
+++ kata_todo/todo/MainController.cs
@@ -0,0 +1,16 @@
+﻿namespace todo
+{
+    internal class MainController
+    {
+        public static void Main(string[] args)
+        {
+            var app = new MainController(args);
+            app.Run();
+        }
+        
+        
+        public MainController(string[] args) {}
+        
+        public void Run() {}
+    }
+}
---
+++ kata_todo/todo_tests/MainController_tests.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using NUnit.Framework;
+using todo;
+
+namespace todo_tests
+{
+    [TestFixture]
+    public class MainController_tests
+    {
+        [Test]
+        public void Run()
+        {
+            var sut = new MainController(new string[0]);
+            sut.Run();
+        }
+    }
+}
