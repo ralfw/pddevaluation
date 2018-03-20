aefbe61be057e0ff1aa4d4b8f1982a335cc459d7
Ralf Westphal <info@ralfw.de>
2018-03-20 16:23:26 +0100

    kata wordcount
    projektgerüst mit test
    
    close #16

---
+++ kata_wordcount/wordcount/Program.cs
@@ -0,0 +1,9 @@
+﻿namespace wordcount
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+        }
+    }
+}
---
+++ kata_wordcount/wordcount_tests/RequestHandler_tests.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using NUnit.Framework;
+
+namespace wordcount_tests
+{
+    [TestFixture]
+    public class RequestHandler_tests
+    {
+        [Test]
+        public void Akteptanztest()
+        {
+            Assert.True(true);
+        }
+    }
+}
