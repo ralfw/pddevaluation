aa2cd17fe001bb3260b8ca004c01189f058cc19e
Ralf Westphal <info@ralfw.de>
2018-03-20 16:36:00 +0100

    Dialoge implementiert
    close #18

---
+++ kata_wordcount/wordcount/Dialog.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace wordcount
+{
+    internal class Dialog
+    {
+        public string Text_erfragen() {
+            Console.Write("Text: ");
+            return Console.ReadLine();
+        }
+
+        public void Ergebnis_anzeigen(int wortanzahl) {
+            Console.WriteLine($"Anzahl Worte im Text: {wordanzahl}");
+        }
+    }
+}
