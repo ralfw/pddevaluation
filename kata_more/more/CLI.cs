82c5d164c1f69473a7c34844979845e582b8ea9f
Ralf Westphal <info@ralfw.de>
2018-03-22 09:52:25 +0100

    CLI #25

---
+++ kata_more/more/CLI.cs
@@ -0,0 +1,12 @@
+﻿namespace more
+{
+    internal class CLI
+    {
+        private readonly string[] _args;
+        public CLI(string[] args) {
+            _args = args;
+        }
+
+        public string Filename => _args[0];
+    }
+}
--- kata_more/more/Program.cs
+++ kata_more/more/Program.cs
@@ -4,6 +4,14 @@
     {
         public static void Main(string[] args)
         {
+            var prg = new Program();
+            prg.Run(args);
+        }
+
+
+        public void Run(string[] args)
+        {
+            var cli = new CLI(args);
         }
     }
 }
